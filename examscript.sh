0fab7d59035448f688b55eeb32e232ed72dff3a9
vladm747 <vladyslav.moroz105@gmail.com>
2020-12-22 21:21:18 +0200

    add examscript.sh

---
+++ examscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n  "Hi, $1"
+echo -n " "
+echo "Hi, $2"
+
+echo "What time is it now?"
+echo "Time: "
+
+day=`date  +%H:%M:%S`
+
+echo "$day"
+
+echo "Creating a directory with the time at the moment..."
+mkdir `date  +%H:%M:%S`
+echo "Directory created:)"
